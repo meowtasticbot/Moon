c2d7459b97f613e21ba1a6cf6a000fb60f8decbc
meowtasticbot <moonop615@gmail.com>
2026-02-24 14:03:06 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -18,12 +18,12 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_3"],url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="ALLBOT_CP"),
+            InlineKeyboardButton(text=_["S_B_10"], callback_data="ALLBOT_CP"),
             InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
         ],
         [
            InlineKeyboardButton(text=_["S_B_5"], callback_data="PROMOTION_CP"),
-            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/shayariAlfaazonKaAaina"),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/Meowstric"),
         ],
 
     ]

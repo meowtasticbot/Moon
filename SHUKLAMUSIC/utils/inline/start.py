3f0efbbecdd51c461318bb6f25eaf00617d71799
meowtasticbot <moonop615@gmail.com>
2026-02-24 13:48:38 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -6,70 +6,25 @@ from SHUKLAMUSIC import app
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
+            InlineKeyboardButton(text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [
-            InlineKeyboardButton(
-                "˹ᴘσʟιᴄʏ˼",
-                url="https://telegra.ph/Privacy-Policy-08-03-101"
-            ),
-            InlineKeyboardButton(
-                "˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪",
-                url="http://t.me/TidalXMusicBot/tidaltunes"
-            ),
-        ],
-        
     ]
     return buttons
-
 
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        
-        [
-            InlineKeyboardButton(
-                "˹ᴘʀιᴠᴧᴄʏ ᴘσʟιᴄʏ˼",
-                url="https://telegra.ph/Privacy-Policy-08-03-101"
-            ),
-            InlineKeyboardButton(
-                "˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪",
-                url="http://t.me/TidalXMusicBot/tidaltunes"
-            ),
+            InlineKeyboardButton(text=_["S_B_3"],url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-        
-            InlineKeyboardButton(
-                text=_["S_B_6"],
-                url=config.SUPPORT_CHANNEL
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_2"],
-                url=config.SUPPORT_CHAT
-            ),
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="ALLBOT_CP"),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
         ],
         [
-    
-            InlineKeyboardButton(
-                text=_["S_B_4"],
-                callback_data="settings_back_helper"
-            ),
+           InlineKeyboardButton(text=_["S_B_5"], callback_data="PROMOTION_CP"),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/shayariAlfaazonKaAaina"),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{config.OWNER_USERNAME}"
-            ),
-        ],
+
     ]
     return buttons
-    
-    
